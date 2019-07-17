5ed2bd6eb55f889faaabd5b81d06f784498631ca
davidedc97 <davidedc1997@gmail.com>
2019-07-17 14:16:37 +0200

    Changes to be committed: 	modified:   Arduino/main.c

--- Arduino/main.c
+++ Arduino/main.c
@@ -2,9 +2,16 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
 #define BAUD 19600
 #define MYUBRR (F_CPU/16/BAUD-1)
+
+volatile uint8_t interrupt_occurred=0;
+
+ISR(TIMER5_COMPA_vect) {
+  interrupt_occurred=1;
+}
 
 void UART_init(void){
   // Set baud rate
@@ -13,13 +20,11 @@ void UART_init(void){
 
   UCSR0C = (1<<UCSZ01) | (1<<UCSZ00); /* 8-bit data */ 
   UCSR0B = (1<<RXEN0) | (1<<TXEN0) | (1<<RXCIE0);   /* Enable RX and TX */  
-
 }
 
 void UART_putChar(uint8_t c){
   // wait for transmission completed, looping on status bit
   while ( !(UCSR0A & (1<<UDRE0)) );
-
   // Start transmission
   UDR0 = c;
 }
@@ -27,14 +32,11 @@ void UART_putChar(uint8_t c){
 uint8_t UART_getChar(void){
   // Wait for incoming data, looping on status bit
   while ( !(UCSR0A & (1<<RXC0)) );
-  
   // Return the data
   return UDR0;
-    
 }
 
-// reads a string until the first newline or 0
-// returns the size read
+// reads a string until the first newline or 0, returns the size read
 uint8_t UART_getString(uint8_t* buf){
   uint8_t* b0=buf; //beginning of buffer
   while(1){
